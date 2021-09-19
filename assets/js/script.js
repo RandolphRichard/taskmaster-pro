00268629fcd25b29829ea9814b8597267378d8aa
Randolph Richard <randolphrichard9@gmail.com>
2021-09-19 15:19:21 -0400

    add the date function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 4) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
 var tasks = {};
 
 var createTask = function(taskText, taskDate, taskList) {
@@ -12,6 +31,9 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+
+    // check due date
+    auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -204,7 +226,15 @@ $(".list-group").on("click", "span", function() {
     .addClass("form-control")
     .val(date);
   $(this).replaceWith(dateInput);
-
+  
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1 ,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the `dateInput`
+      $(this).trigger("change");
+    }
+  });
   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
@@ -231,7 +261,11 @@ $(".list-group").on("change", "input[type='text']", function() {
     .addClass("badge badge-primary badge-pill")
     .text(date);
     $(this).replaceWith(taskSpan);
+
+     // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
+
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
@@ -245,3 +279,8 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
